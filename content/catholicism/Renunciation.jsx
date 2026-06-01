5f8fa4f66907bf5fbd28f665c9cf4a25ec8a8590
Jake Miu <chinchuck101@gmail.com>
2026-06-01 16:40:39 -0400

    renunciation: first third written, needs spacing fixes tuned to google doc, and globals.css updated with table customization done

--- content/catholicism/Renunciation.jsx
+++ content/catholicism/Renunciation.jsx
@@ -1,11 +1,150 @@
 export default function Renunciation() {
     return (
         <>
-            <h1>Renunciation</h1>
+            <h1>Renunciation of Satan</h1>
             
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Recall from the section overview page that before the elect get baptized, part of the baptismal rite includes renouncing Satan.
             </p>
+            
+            <p>
+                Specifically, the elect are asked to renounce:
+            </p>
+            
+            <ul>
+                <li>Satan</li>
+                <li>All his works</li>
+                <li>All his pomps</li>
+            </ul>
+
+            <p>
+                We’ll dive into what exactly renouncing each of the above means below.
+            </p>
+
+            <br />
+            <hr />
+            <h2><u>Renouncing Satan</u></h2>
+
+            <h3>What does “satan” mean? How did it become “The Devil”?</h3>
+            <p>
+                The Hebrew word satan originally translates to "adversary," "opponent," or "accuser."
+                In the Old Testament, it was used as a common noun.
+                However, when written as ha-satan, it referred specifically to the Angelic Prosecutor who tests humanity, as seen in the Book of Job.
+                By the time the New Testament was written, the word had evolved into the proper noun: Satan - referring directly to the Devil.
+                <br /><br />
+                Over the next two millennia, the bible was translated into newer languages and eventually Satan was referred to as “The Devil” following this general path:
+            </p>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Language</th>
+                        <th>Word</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Hebrew</td>
+                        <td>Satan</td>
+                    </tr>
+                    <tr>
+                        <td>Ancient Greek</td>
+                        <td>Diabolos</td>
+                    </tr>
+                    <tr>
+                        <td>Latin</td>
+                        <td>Diabolus</td>
+                    </tr>
+                    <tr>
+                        <td>Medieval English</td>
+                        <td>Deoful</td>
+                    </tr>
+                    <tr>
+                        <td>Modern English</td>
+                        <td>Devil</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <p>
+                Satan became the angel’s name, and “The Devil” became his title.
+            </p>
+
+            <h3>Who is Satan?</h3>
+            <p>
+                To describe who Satan is, we can break down his alternate name - Lucifer Morningstar.
+            </p>
+
+            <h4><u>Morning Star</u></h4>
+            <p>
+                Because Venus shines at its brightest in the night sky just before the sun rises, it was historically referred to as the “Morning Star”, as if it was announcing that morning was right around the corner.
+                For the purposes of this metaphor, it’s important to point out that the shine on Venus is a reflection of the light coming from the sun, not from Venus itself
+                In the Old Testament in Isaiah 14:12, the King of Babylon was called “Shining One, Son of the Dawn”.
+                It was used as a poetic insult to mock a proud king, who thought he was as brilliant and high as the morning star, but was ultimately cast down and eclipsed.
+            </p>
+
+            <h4><u>Lucifer</u></h4>
+            <p>
+                "Light-bearer" is the literal translation of the Latin word Lucifer.
+            </p>
+            <ul>
+                <li>The Etymology: When the Bible was translated from Greek into Latin, translators took the Greek word for the morning star, and translated it into the Latin equivalent: Lucifer (lux = light, ferre = to bear/bring).</li>
+                <li>
+                    The Meaning: Before his fall, Lucifer was the ultimate "Light-Bearer" because he was the most radiant and intelligent creature in the cosmos.
+                    However, he did not possess his own independent light; rather, he perfectly reflected the light and glory of God, much like Venus has no light of its own but brightly reflects the light of the sun.
+                </li>
+            </ul>
+            <p>
+                Over time, people took both “Lucifer” and “Morning Star” and have aptly applied it to Satan.
+                <br /><br />
+                In order to point out the irony of this name, we have to explain Satan’s rebellion against God, which is why he was cast out of heaven.
+            </p>
+
+            <h3><u>The Rebellion: What Happened?</u></h3>
+            <p>
+                If angels reside in heaven with God, how does a rebellion even happen?
+                According to Catholic theology, angels are pure spirits blessed with incredible intellect and completely free will.
+                At the moment of their creation, God gave the angels a choice to test their fidelity before they could enter the Fullness of His Presence (the Beatific Vision).
+                We do not know the exact nature of the test, but Church tradition suggests two main theological reasons for the coup:
+            </p>
+            
+            <ul>
+                <li><strong>The Sin of Pride:</strong> Lucifer looked upon his own immense beauty and wisdom and became infatuated with himself.</li>
+                <li>He decided he wanted to attain ultimate happiness and glory by his own natural power, rather than humbly receive it as a gift of grace from God.</li>
+                <li><strong>The Refusal to Serve:</strong> Tradition also suggests that when God revealed His ultimate plan, that the angels would eventually have to serve and bow before God-made-man (Jesus Christ) and honor His human Mother (Mary), Lucifer rebelled.</li>
+                <li>He found the idea of serving lower, physical human beings utterly insulting to his superior angelic nature.</li>
+            </ul>
+
+            <p>
+                Lucifer rallied a third of the angels to his side, initiating a cosmic battle of wills.
+                It was not a physical war, but a definitive, irreversible choice of allegiance.
+                Lucifer's battle cry was <em>"Non serviam"</em>, Latin for <strong>"I will not serve."</strong>
+            </p>
+
+            <h3>The Irony of the Name</h3>
+            <p>
+                The malice of the devil’s rebellion lies in his deliberate refusal to acknowledge that his light was merely a gift.
+                In his pride, he demanded to be worshipped as the source of light rather than serve as the bearer of it.
+                By choosing radical rebellion, he utterly corrupted his own nature, thereby forfeiting his role as a reflection of God’s glory to become the architect of spiritual darkness.
+            </p>
+
+            <h3>So what does it mean to renounce Satan?</h3>
+            <p>
+                You are pledging allegiance to God instead of Satan.
+                After being cast out of heaven for his rebellion, Satan is actively working to thwart and deceive humanity.
+                To understand how he attacks us daily, let’s look at his tactics.
+            </p>
+
+            <br /><br />
+            <hr />
+            <h2><u>… and all his works</u></h2>
+            <p>
+                1 Peter 5:8: "Be alert and of sober mind. Your enemy the devil prowls around like a roaring lion looking for someone to devour."
+            </p>
+
+            <br /><br />
+            <hr />
+            <h2><u>… and all his pomps</u></h2>
         </>
     );
 }
